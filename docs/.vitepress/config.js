10ffea4c7733c3ebf9ec97a3c82c74f523397975
fanwei <fanwei@e6yun.com>
2023-05-04 18:49:37 +0800

    文档配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@
  * @modlue: 
  * @Author: fanwei
  * @Date: 2023-04-28 16:10:56
- * @LastEditTime: 2023-05-04 10:58:00
+ * @LastEditTime: 2023-05-04 18:46:49
  * @LastEditors: fanwei
  */
 export default {
@@ -11,6 +11,25 @@ export default {
     description: 'Vue 3.x',
     themeConfig: {
         logo: '/logo.png',
+        search: {
+            provider: 'local',
+            placeholder: '搜索',
+            background: '#fff',
+
+        },
+        outlineTitle: '目录',
+        lastUpdated: true,
+        sidebar: {
+            '/guide/': [
+                {
+                    text: '开始导航',
+                    collapsed: false,
+                    items: [
+                        { text: '首页', link: '/' },
+                      ]
+                }
+            ]
+        },
         nav: [
             {
                 text: '首页',
