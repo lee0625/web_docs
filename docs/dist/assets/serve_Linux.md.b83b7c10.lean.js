14f7d6cd7d0b44207a09a5b0a9d50bba17b5d3a3
fanwei <fanwei@e6yun.com>
2023-05-04 14:00:31 +0800

    静态文件测试

---
+++ docs/dist/assets/serve_Linux.md.b83b7c10.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as i,o as l,O as e}from"./chunks/framework.8012a439.js";const x=JSON.parse('{"title":"Linux速查宝典","description":"","frontmatter":{},"headers":[],"relativePath":"serve/Linux.md"}'),t={name:"serve/Linux.md"},r=e("",13),o=[r];function n(h,s,d,c,u,_){return l(),i("div",null,o)}const f=a(t,[["render",n]]);export{x as __pageData,f as default};
