818d39d0e4ffffd5286ae86cff3345dff2b70d1a
fanwei <fanwei@e6yun.com>
2023-05-04 10:48:09 +0800

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,33 @@
+/*
+ * @modlue: 
+ * @Author: fanwei
+ * @Date: 2023-04-28 16:10:56
+ * @LastEditTime: 2023-04-28 16:50:01
+ * @LastEditors: fanwei
+ */
+export default {
+    title: '码客笔记',
+    description: 'Vue 3.x',
+    themeConfig: {
+        logo: '/logo.png',
+        nav: [
+            {
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: '服务端',
+                items: [
+                    {
+                        text: 'Linux',
+                        link: '/serve/Linux'
+                    }
+                ]
+            },
+            {
+                text: '关于',
+                link: '/about'
+            }
+        ]
+    }
+}
---
+++ docs/index.md
@@ -0,0 +1,32 @@
+---
+layout: home
+
+title: 码客笔记
+titleTemplate: 开发笔记，让开发更轻松
+
+hero:
+  name: 码客笔记
+  text: 让开发更轻松
+  tagline: 没啥特点仅供学习查阅
+  image:
+    src: /logo.png
+    alt: 码客笔记
+  actions:
+    - theme: brand
+      text: 开始
+      link: /guide/
+    - theme: alt
+      text: 在 Gitee 上查看
+      link: https://gitee.com/geeksdidi/kittyui
+
+features:
+  - icon: 💡
+    title: Vue3组件库
+    details: 基于vite打包和TypeScript开发
+  - icon: 📦
+    title: 仅供学习使用
+    details: 倾向于Vue3组件库的学习，请勿用于实际生产项目
+  - icon: 🛠️
+    title: 按需引入
+    details: 直接支持按需引入无需配置任何插件。
+---
