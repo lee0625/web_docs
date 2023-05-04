f685ccf1b2ef262023bfac0ef00af8313bd80b98
fanwei <fanwei@e6yun.com>
2023-05-04 11:39:23 +0800

    自动化脚本

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,11 +2,12 @@
  * @modlue: 
  * @Author: fanwei
  * @Date: 2023-04-28 16:10:56
- * @LastEditTime: 2023-04-28 16:50:01
+ * @LastEditTime: 2023-05-04 10:58:00
  * @LastEditors: fanwei
  */
 export default {
     title: '码客笔记',
+    base: '/web_docs/',
     description: 'Vue 3.x',
     themeConfig: {
         logo: '/logo.png',
