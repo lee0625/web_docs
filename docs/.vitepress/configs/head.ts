3bdee38fd93d9f568931bb9ebb1c1d9e630f4d2d
fanwei <fanwei@e6yun.com>
2023-05-08 14:30:23 +0800

    logo修改

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -2,13 +2,13 @@
  * @modlue: 
  * @Author: fanwei
  * @Date: 2023-05-05 10:41:59
- * @LastEditTime: 2023-05-05 10:53:43
+ * @LastEditTime: 2023-05-08 14:29:46
  * @LastEditors: fanwei
  */
 import type { HeadConfig } from 'vitepress'
 
 export const head: HeadConfig[] = [
-    ['link', { rel: 'icon', href: '/logo.png' }],
-    ['link', { rel: 'apple-touch-icon', href: '/logo.png' }],
-    ['link', { rel: 'mask-icon', href: '/logo.png', color: '#3eaf7c' }],
+    ['link', { rel: 'icon', href: '../logo.png' }],
+    ['link', { rel: 'apple-touch-icon', href: '../logo.png' }],
+    ['link', { rel: 'mask-icon', href: '../logo.png', color: '#3eaf7c' }],
 ]
